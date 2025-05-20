aaa07f8d367fc1922b36a134fe9e269c44428a5a
Shreedhar T <166901066+visionimpacttech@users.noreply.github.com>
2025-05-20 18:25:27 +0530

    Update example_whatsapp_bot.py

--- example_whatsapp_bot.py
+++ example_whatsapp_bot.py
@@ -1,8 +1,9 @@
 
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-from bot_engine import get_response
-from user_profile import UserProfile
+from .user_profile import UserProfile
+from .bot_engine import get_response
+
 
 app = Flask(__name__)
 
